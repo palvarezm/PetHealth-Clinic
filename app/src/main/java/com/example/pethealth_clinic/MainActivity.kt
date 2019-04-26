75ac05e9cb09f2ce2e7c1f66ca0af66632bffda3
Developer <developer@B606-09.upc.edu.pe>
2019-04-26 11:01:37 -0500

    commit

--- app/src/main/java/com/example/pethealth_clinic/MainActivity.kt
+++ app/src/main/java/com/example/pethealth_clinic/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.pethealth_clinic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val host = NavHostFragment.create(R.navigation.nav_graph)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container,host)
+            .setPrimaryNavigationFragment(host)
+            .commit()
     }
 }
